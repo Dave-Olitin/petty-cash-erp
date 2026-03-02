e06e6a39664d6f9dc0a9e80f346b0a8550c78273
Dave Dev <Davemelgazoolitin2000@gmail.com>
2026-03-02 11:37:08 +0400

    Fixing issues

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,8 +13,9 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->validateCsrfTokens(except: [
-            '/push/subscribe',
-            '/push/unsubscribe',
+            'push/subscribe',
+            'push/unsubscribe',
+            'push/*'
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
